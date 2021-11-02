912e0f7ad0d8adfcdff20a209e49a0d2bcf4357e
mremmalex <mremmalex2@gmail.com>
2021-11-02 17:27:12 +0100

    worked on the withdrawal process

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ChakraProvider} from "@chakra-ui/react";
 import Home from "./pages/home/Home";
 import Pay from "./pages/pay/Pay";
+import Payout from "./pages/withdrawal/Payout";
+
 import './App.css';
 
 function App() {
@@ -10,7 +12,8 @@ function App() {
     < ChakraProvider>
       <Router>
         <Route path='/' component={Home} exact/>
-        <Route path="/pay/:id/:amount" component={Pay} />
+        <Route path="/deposit/:id/:amount" component={Pay} />
+        <Route path="/payout/:wdid/:id/:amount/:userwallet" component={Payout} />
       </Router>
     </ChakraProvider>
   );
